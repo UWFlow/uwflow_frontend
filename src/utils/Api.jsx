c2cdf5b1d95973fd77051dd87f2e5d09dd9a7876
Edwin Zhang <edwinzhang64@gmail.com>
2019-09-02 18:38:21 -0600

    Set token in localStorage on auth request

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,6 +1,16 @@
-export const externalCall = (endpoint, data) =>
-  fetch({
-    method: 'post',
-    url: endpoint,
-    data: JSON.stringify(data),
-  });
+// makes POST request to endpoint
+// returns response body and status as array
+export const makePOSTRequest = async (endpoint, data) => {
+  const res = await fetch(
+    endpoint,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    }
+  );
+  return [await res.json(), await res.status];
+}
