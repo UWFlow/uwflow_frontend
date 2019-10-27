d757c7bf95fd8166bf5717ad29297755bba9b3df
CatastrophiClam <jdaimax@gmail.com>
2019-10-27 11:09:08 -0700

    Cleaned up review flow

--- src/auth/ResetPasswordModal.jsx
+++ src/auth/ResetPasswordModal.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import ModalHOC from '../sharedComponents/modal/ModalHOC';
 import Textbox from '../sharedComponents/input/Textbox';
 import Button from '../sharedComponents/input/Button';
+import LoadingSpinner from '../sharedComponents/display/LoadingSpinner';
 
 /* Utils */
 import { makePOSTRequest } from '../utils/Api';
@@ -17,15 +18,25 @@ import {
 } from '../constants/Api';
 
 /* Styled Components */
-import { Wrapper, Header, Error } from './styles/ResetPasswordModal';
-import { TextboxWrapper } from './styles/ResetPasswordModal';
+import {
+  Wrapper,
+  Header,
+  Text,
+  Error,
+  Success,
+  TextboxWrapper,
+  GreyLink,
+  LoadingSpinnerWrapper,
+} from './styles/ResetPasswordModal';
 
-const ResetPasswordForm = ({ onSubmit, error }) => {
+const ResetPasswordForm = ({ onSubmit, loading, error, success }) => {
   const [email, setEmail] = useState('');
+  console.log(loading);
   return (
     <Wrapper>
       <Header>Reset Password</Header>
       {error !== '' && <Error>{error}</Error>}
+      {success !== '' && <Success>{success}</Success>}
       <TextboxWrapper>
         <Textbox
           options={{ width: '100%', type: 'email' }}
@@ -37,19 +48,35 @@ const ResetPasswordForm = ({ onSubmit, error }) => {
           }}
         />
       </TextboxWrapper>
-      <Button width="100%" handleClick={() => onSubmit(email)}>
-        Send Reset Email
-      </Button>
+      {loading ? (
+        <LoadingSpinnerWrapper>
+          <LoadingSpinner size={24} margin="" />
+        </LoadingSpinnerWrapper>
+      ) : (
+        <Button width="100%" handleClick={() => onSubmit(email)}>
+          Send Reset Email
+        </Button>
+      )}
     </Wrapper>
   );
 };
 
-const EnterResetCodeForm = ({ onSubmit, error }) => {
+const EnterResetCodeForm = ({
+  onSubmit,
+  loading,
+  error,
+  resendEmail,
+  success,
+}) => {
   const [code, setCode] = useState('');
   return (
     <Wrapper>
       <Header>Enter reset code</Header>
+      <Text>
+        We just sent you an email with a reset code, please enter it below:
+      </Text>
       {error !== '' && <Error>{error}</Error>}
+      {success !== '' && <Success>{success}</Success>}
       <TextboxWrapper>
         <Textbox
           options={{ width: '100%' }}
@@ -60,24 +87,32 @@ const EnterResetCodeForm = ({ onSubmit, error }) => {
             setCode(value);
           }}
         />
+        <GreyLink onClick={resendEmail}>Resend email</GreyLink>
       </TextboxWrapper>
-      <Button width="100%" handleClick={() => onSubmit(code)}>
-        Submit
-      </Button>
+      {loading ? (
+        <LoadingSpinnerWrapper>
+          <LoadingSpinner size={24} margin="" />
+        </LoadingSpinnerWrapper>
+      ) : (
+        <Button width="100%" handleClick={() => onSubmit(code)}>
+          Submit
+        </Button>
+      )}
     </Wrapper>
   );
 };
 
-const EnterNewPasswordForm = ({ onSubmit, error }) => {
+const EnterNewPasswordForm = ({ onSubmit, loading, error, success }) => {
   const [pass, setPass] = useState('');
   const [confirmPass, setConfirmPass] = useState('');
   return (
     <Wrapper>
       <Header>Enter new password</Header>
       {error !== '' && <Error>{error}</Error>}
+      {success !== '' && <Success>{success}</Success>}
       <TextboxWrapper>
         <Textbox
-          options={{ width: '100%' }}
+          options={{ width: '100%', type: 'password' }}
           placeholder="New password"
           error={false}
           text={pass}
@@ -88,7 +123,7 @@ const EnterNewPasswordForm = ({ onSubmit, error }) => {
       </TextboxWrapper>
       <TextboxWrapper>
         <Textbox
-          options={{ width: '100%' }}
+          options={{ width: '100%', type: 'password' }}
           placeholder="Confirm new password"
           error={false}
           text={confirmPass}
@@ -98,7 +133,7 @@ const EnterNewPasswordForm = ({ onSubmit, error }) => {
         />
       </TextboxWrapper>
       <Button width="100%" handleClick={() => onSubmit(pass, confirmPass)}>
-        Send Reset Email
+        Reset Password
       </Button>
     </Wrapper>
   );
@@ -107,16 +142,20 @@ const EnterNewPasswordForm = ({ onSubmit, error }) => {
 const RESET_PASSWORD_FORM = 'RESET_PASSWORD';
 const ENTER_RESET_CODE_FORM = 'RESET_CODE';
 const ENTER_NEW_PASSWORD_FORM = 'NEW_PASSWORD';
+const TIMEOUT_LENGTH = 750;
 
 const ResetPasswordModal = ({ handleClose, isOpen }) => {
   const [showingForm, setShowingForm] = useState(RESET_PASSWORD_FORM);
   const [savedCode, setCode] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
 
   const handleSendResetEmail = async email => {
     setIsLoading(true);
     setErrorMessage('');
+    setSuccessMessage('');
     const [response, status] = await makePOSTRequest(
       `${BACKEND_ENDPOINT}${RESET_PASSWORD_KEY_EMAIL_ENDPOINT}`,
       { email: email },
@@ -127,16 +166,24 @@ const ResetPasswordModal = ({ handleClose, isOpen }) => {
       setErrorMessage(response.error);
     } else {
       // SUCCESS
-      setShowingForm(ENTER_RESET_CODE_FORM);
+      setEmail(email);
+      setSuccessMessage('Successfully sent reset code!');
+      if (showingForm != ENTER_RESET_CODE_FORM) {
+        setTimeout(() => {
+          setSuccessMessage('');
+          setShowingForm(ENTER_RESET_CODE_FORM);
+        }, TIMEOUT_LENGTH);
+      }
     }
   };
 
   const handleSubmitResetCode = async code => {
     setIsLoading(true);
     setErrorMessage('');
+    setSuccessMessage('');
     const [response, status] = await makePOSTRequest(
-      `${BACKEND_ENDPOINT}${RESET_PASSWORD_VERIFY_KEY_ENDPOINT}`,
-      { key: code },
+      `${BACKEND_ENDPOINT}${RESET_PASSWORD_VERIFY_KEY_ENDPOINT}?key=${code}`,
+      {},
     );
     setIsLoading(false);
     if (status >= 400) {
@@ -145,7 +192,11 @@ const ResetPasswordModal = ({ handleClose, isOpen }) => {
     } else {
       // SUCCESS
       setCode(code);
-      setShowingForm(ENTER_NEW_PASSWORD_FORM);
+      setSuccessMessage('Code is valid!');
+      setTimeout(() => {
+        setSuccessMessage('');
+        setShowingForm(ENTER_NEW_PASSWORD_FORM);
+      }, TIMEOUT_LENGTH);
     }
   };
 
@@ -156,6 +207,7 @@ const ResetPasswordModal = ({ handleClose, isOpen }) => {
     }
     setIsLoading(true);
     setErrorMessage('');
+    setSuccessMessage('');
     const [response, status] = await makePOSTRequest(
       `${BACKEND_ENDPOINT}${RESET_PASSWORD_RESET_PASSWORD_ENDPOINT}`,
       { key: savedCode, password: newPass },
@@ -166,7 +218,11 @@ const ResetPasswordModal = ({ handleClose, isOpen }) => {
       setErrorMessage(response.error);
     } else {
       // SUCCESS
-      handleClose();
+      setSuccessMessage('Password successfully reset!');
+      setTimeout(() => {
+        setSuccessMessage('');
+        handleClose();
+      }, TIMEOUT_LENGTH);
     }
   };
 
@@ -175,19 +231,26 @@ const ResetPasswordModal = ({ handleClose, isOpen }) => {
       {showingForm === RESET_PASSWORD_FORM && (
         <ResetPasswordForm
           onSubmit={handleSendResetEmail}
+          loading={isLoading}
           error={errorMessage}
+          success={successMessage}
         />
       )}
       {showingForm === ENTER_RESET_CODE_FORM && (
         <EnterResetCodeForm
           onSubmit={handleSubmitResetCode}
+          loading={isLoading}
           error={errorMessage}
+          resendEmail={() => handleSendResetEmail(email)}
+          success={successMessage}
         />
       )}
       {showingForm === ENTER_NEW_PASSWORD_FORM && (
         <EnterNewPasswordForm
           onSubmit={handleNewPassword}
+          loading={isLoading}
           error={errorMessage}
+          success={successMessage}
         />
       )}
     </ModalHOC>
