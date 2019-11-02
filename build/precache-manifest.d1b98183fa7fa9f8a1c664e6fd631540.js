1eb902417978113960c9576e4054bf337ccbf7c7
Edwin Zhang <edwinzhang64@gmail.com>
2019-11-01 21:45:38 -0700

    Add build folder

---
+++ build/precache-manifest.d1b98183fa7fa9f8a1c664e6fd631540.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a4a18e08c8d967d8158cbc6609b05b",
+    "url": "/cc98749884bb6cdc0b1b.worker.js"
+  },
+  {
+    "revision": "1db08c3f55af755ca603a1a7063acccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "213c2e66f29a09462799",
+    "url": "/static/js/0.74c36f95.chunk.js"
+  },
+  {
+    "revision": "89216e38557acad71c5d",
+    "url": "/static/js/1.9e93049d.chunk.js"
+  },
+  {
+    "revision": "7c7d7a5ad1d68227a5d3",
+    "url": "/static/js/10.0a4dabb8.chunk.js"
+  },
+  {
+    "revision": "a8cfae53741dee6ca1c1",
+    "url": "/static/js/11.9ff2d803.chunk.js"
+  },
+  {
+    "revision": "44b7d4c4faf19248b9f5",
+    "url": "/static/js/12.dda9527f.chunk.js"
+  },
+  {
+    "revision": "d9798725ae5aa4d363ec",
+    "url": "/static/js/13.8b6cb8b0.chunk.js"
+  },
+  {
+    "revision": "c8a13132c8de9a085877",
+    "url": "/static/js/14.a64796f6.chunk.js"
+  },
+  {
+    "revision": "5ce998c068f9e467ac42",
+    "url": "/static/js/2.2ab3ee01.chunk.js"
+  },
+  {
+    "revision": "a4bb8819ec12e0ae127c",
+    "url": "/static/js/5.8fec7d8c.chunk.js"
+  },
+  {
+    "revision": "50d6e1a414836fcd62df",
+    "url": "/static/js/6.43c42026.chunk.js"
+  },
+  {
+    "revision": "f30dfa4214617a9fa30e",
+    "url": "/static/js/7.2683b451.chunk.js"
+  },
+  {
+    "revision": "e558107a1758b2858819",
+    "url": "/static/js/8.aa811ef6.chunk.js"
+  },
+  {
+    "revision": "7cf1a0969ed1ffbe6df9",
+    "url": "/static/js/9.38293855.chunk.js"
+  },
+  {
+    "revision": "7e4a51fa5655eafbb35c",
+    "url": "/static/js/main.8cf03b12.chunk.js"
+  },
+  {
+    "revision": "bfe4646f8860b83039fb",
+    "url": "/static/js/runtime-main.3cce0906.js"
+  },
+  {
+    "revision": "4d2c3421f70d0fe5ff8dff92308f4dfb",
+    "url": "/static/media/background.4d2c3421.png"
+  }
+]);
