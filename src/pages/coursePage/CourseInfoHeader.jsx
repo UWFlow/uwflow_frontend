ca85cead7a7d5eabcba7575f9c3969acc6c5755d
CatastrophiClam <jdaimax@gmail.com>
2019-11-05 21:24:08 -0800

    Copy desktop coursePage stuff to pages folder

---
+++ src/pages/coursePage/CourseInfoHeader.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+/* Styled Components */
+import {
+  CourseInfoHeaderWrapper,
+  CourseCodeAndNameSection,
+  CourseDescriptionSection,
+  CourseCodeAndNameWrapper,
+  CourseCode,
+  CourseName,
+  Description,
+  RatingsSection,
+  CourseCodeAndStar,
+  StarAlignmentWrapper,
+} from './styles/CourseInfoHeader';
+
+/* Child Components */
+import RatingBox, {
+  RATING_BOX_WIDTH,
+} from '../../components/display/RatingBox';
+import ShortlistStar from '../../components/input/ShortlistStar';
+
+import { splitCourseCode } from '../../../utils/Misc';
+import { isLoggedIn } from '../../../utils/Auth';
+
+const CourseInfoHeader = ({ course, shortlisted, setAuthModalOpen }) => {
+  const { liked, easy, useful } = course.course_reviews_aggregate.aggregate.avg;
+  const { count, text_count } = course.course_reviews_aggregate.aggregate;
+  const [isStarClicked, setIsStarClicked] = useState(shortlisted);
+
+  const onStarClick = () => {
+    isLoggedIn() ? setIsStarClicked(!isStarClicked) : setAuthModalOpen(true);
+  };
+
+  return (
+    <CourseInfoHeaderWrapper>
+      <CourseCodeAndNameSection>
+        <CourseCodeAndNameWrapper>
+          <CourseCodeAndStar>
+            <CourseCode ratingBoxWidth={RATING_BOX_WIDTH}>
+              {splitCourseCode(course.code)}
+            </CourseCode>
+            <StarAlignmentWrapper>
+              <ShortlistStar
+                size={36}
+                checked={isStarClicked}
+                onClick={onStarClick}
+              />
+            </StarAlignmentWrapper>
+          </CourseCodeAndStar>
+          <CourseName ratingBoxWidth={RATING_BOX_WIDTH}>
+            {course.name}
+          </CourseName>
+        </CourseCodeAndNameWrapper>
+      </CourseCodeAndNameSection>
+      <CourseDescriptionSection>
+        <RatingsSection>
+          <RatingBox
+            numRatings={count}
+            numComments={text_count}
+            percentages={[
+              {
+                displayName: 'Likes',
+                percent: liked,
+              },
+              {
+                displayName: 'Useful',
+                percent: useful / 5,
+              },
+              {
+                displayName: 'Easy',
+                percent: easy / 5,
+              },
+            ]}
+          />
+        </RatingsSection>
+        <Description ratingBoxWidth={RATING_BOX_WIDTH}>
+          {course.description}
+        </Description>
+      </CourseDescriptionSection>
+    </CourseInfoHeaderWrapper>
+  );
+};
+
+CourseInfoHeader.propTypes = {
+  course: PropTypes.object.isRequired,
+};
+
+export default CourseInfoHeader;
---
+++ src/pages/coursePage/CourseScheduleTableColumns.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import {
+  SectionCellWrapper,
+  SectionContentWrapper,
+  NormalCellWrapper,
+  ContentWrapper,
+  InstructorLink,
+  ColorBar,
+} from './styles/CourseSchedule';
+import { getProfPageRoute } from '../../../Routes';
+
+export const LEC = 'LEC';
+export const LAB = 'LAB';
+export const TUT = 'TUT';
+
+const SectionCell = ({ cell }) => (
+  <SectionCellWrapper numRows={cell.value.numRows}>
+    <ColorBar
+      color={
+        cell.value.section.includes(LEC)
+          ? LEC
+          : cell.value.section.includes(LAB)
+          ? LAB
+          : TUT
+      }
+    />
+    <SectionContentWrapper>{cell.value.section}</SectionContentWrapper>
+  </SectionCellWrapper>
+);
+
+const ClassCell = ({ cell }) => (
+  <NormalCellWrapper>{cell.value}</NormalCellWrapper>
+);
+const EnrolledCell = ({ cell }) => (
+  <NormalCellWrapper>
+    {cell.value.filled}/{cell.value.capacity}
+  </NormalCellWrapper>
+);
+const TimeCell = ({ cell }) => (
+  <NormalCellWrapper>
+    {cell.value.map((cl, ind) => {
+      return (
+        <ContentWrapper key={ind}>
+          {cl.time.start} - {cl.time.end}
+        </ContentWrapper>
+      );
+    })}
+  </NormalCellWrapper>
+);
+const DateCell = ({ cell }) => (
+  <NormalCellWrapper>
+    {cell.value.map((cl, ind) => {
+      return (
+        <ContentWrapper key={ind}>
+          {cl.date.reduce((acc, curr, ind) => {
+            return `${acc}${ind === 0 ? curr : ` ${curr}`}`;
+          }, '')}
+        </ContentWrapper>
+      );
+    })}
+  </NormalCellWrapper>
+);
+const LocationCell = ({ cell }) => (
+  <NormalCellWrapper>
+    {cell.value.map((cl, ind) => {
+      return <ContentWrapper key={ind}>{cl.location}</ContentWrapper>;
+    })}
+  </NormalCellWrapper>
+);
+const InstructorCell = ({ cell }) => (
+  <NormalCellWrapper>
+    {cell.value.map((cl, ind) => {
+      if (!cl.instructor) return <ContentWrapper key={ind} />;
+      return (
+        <InstructorLink
+          to={getProfPageRoute(cl.instructor.toString())}
+          key={ind}
+        >
+          {cl.instructor}
+        </InstructorLink>
+      );
+    })}
+  </NormalCellWrapper>
+);
+
+export const courseScheduleTableColumns = [
+  {
+    Header: 'Section',
+    Cell: SectionCell,
+    accessor: 'section',
+    style: {
+      padding: 0,
+    },
+  },
+  {
+    Header: 'Class',
+    Cell: ClassCell,
+    accessor: 'class_number',
+    style: {
+      'vertical-align': 'top',
+    },
+  },
+  {
+    Header: 'Enrolled',
+    Cell: EnrolledCell,
+    accessor: 'enrolled',
+    style: {
+      'vertical-align': 'top',
+    },
+  },
+  {
+    Header: 'Time',
+    Cell: TimeCell,
+    accessor: 'classes',
+  },
+  {
+    Header: 'Date',
+    Cell: DateCell,
+    accessor: 'classes',
+  },
+  {
+    Header: 'Location',
+    Cell: LocationCell,
+    accessor: 'classes',
+  },
+  {
+    Header: 'Instructor',
+    Cell: InstructorCell,
+    accessor: 'classes',
+  },
+];
