f0aef706b0a3cf165f9272f034d84a4a596d02a4
Edwin Zhang <edwinzhang64@gmail.com>
2019-11-10 19:59:49 -0800

    Remove explore page switch

--- src/pages/explorePage/styles/ExplorePage.jsx
+++ src/pages/explorePage/styles/ExplorePage.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
 import { Link as RouterLink } from 'react-router-dom';
 import ExploreHeader from '../../../img/explore_v1.svg';
 
@@ -21,13 +22,17 @@ export const ExploreHeaderWrapper = styled.div`
   background-size: cover;
   flex-direction: column;
   position: relative;
+
+  ${breakpoint('mobile', 'tablet')`
+    padding: 0 16px;
+  `}
 `;
 
 export const ExploreHeaderText = styled.div`
   ${PageContent}
   ${Heading2}
-  padding-top: 48px !important;
-  padding-bottom: 16px !important;
+  padding-top: 48px;
+  padding-bottom: 16px;
   word-break: break-all;
   min-height: 104px;
   display: flex;
@@ -43,14 +48,26 @@ export const ColumnWrapper = styled.div`
   ${PageContent}
   margin: auto;
   display: flex;
+  flex-flow: row wrap;
 `;
 
 export const Column1 = styled.div`
   ${WideColumn}
+
+  ${breakpoint('mobile', 'tablet')`
+    width: 100%;
+    padding: 0;
+    order: 2;
+  `}
 `;
 
 export const Column2 = styled.div`
   ${ThinColumn}
+
+  ${breakpoint('mobile', 'tablet')`
+    width: 100%;
+    order: 1;
+  `}
 `;
 
 export const CourseCode = styled(RouterLink)`
