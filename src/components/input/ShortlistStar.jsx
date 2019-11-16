e4d754fa74a432fc52bc49cdefa5836b70176391
Edwin Zhang <edwinzhang64@gmail.com>
2019-11-16 01:38:36 -0800

    Add shortlist star mutation

--- src/components/input/ShortlistStar.jsx
+++ src/components/input/ShortlistStar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withTheme } from 'styled-components';
+import { useMutation } from 'react-apollo';
 
 /* Styled Components */
 import { ShortlistStarWrapper } from './styles/ShortlistStar';
@@ -10,19 +11,31 @@ import { ShortlistStarWrapper } from './styles/ShortlistStar';
 import { getIsLoggedIn } from '../../data/reducers/AuthReducer';
 import { authModalOpen } from '../../data/actions/AuthActions';
 
+/* Mutations */
+import { DELETE_USER_SHORTLIST, INSERT_USER_SHORTLIST } from '../../graphql/mutations/user/Shortlist';
+
 const mapStateToProps = state => ({
   isLoggedIn: getIsLoggedIn(state),
 });
 
 const ShortlistStar = ({ theme, courseID, isLoggedIn, initialState = false, size = 32 }) => {
-  const [checked, setChecked] = useState(initialState);
   const dispatch = useDispatch();
+  const [checked, setChecked] = useState(initialState);
+  const [insertShortlist] = useMutation(INSERT_USER_SHORTLIST);
+  const [deleteShortlist] = useMutation(DELETE_USER_SHORTLIST);
 
   const onStarClicked = () => {
     if (!isLoggedIn) {
       dispatch(authModalOpen());
     } else {
-      return;
+      const userID = localStorage.getItem('user_id');
+      const mutationVariables = { variables: { user_id: userID, course_id: courseID } };
+      if (checked) {
+        deleteShortlist(mutationVariables);
+      } else {
+        insertShortlist(mutationVariables);
+      }
+      setChecked(!checked);
     }
   }
 
