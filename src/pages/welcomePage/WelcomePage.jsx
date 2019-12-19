6090cbb2e9006798f2e4a5b96969a1a1ea1b3d8e
Edwin Zhang <edwinzhang64@gmail.com>
2019-12-18 22:10:23 -0800

    Redirect welcome page when not logged in

--- src/pages/welcomePage/WelcomePage.jsx
+++ src/pages/welcomePage/WelcomePage.jsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 /* Styled Components */
-import { WelcomePageWrapper } from './styles/welcomePage';
-
-/* Constants */
-import { LANDING_PAGE_ROUTE } from '../../Routes';
+import { WelcomePageWrapper } from './styles/WelcomePage';
 
 /* Child Components */
 import TranscriptUploadModalContent from '../../components/dataUploadModals/TranscriptUploadModalContent';
 import ScheduleUploadModalContent from '../../components/dataUploadModals/ScheduleUploadModalContent';
 
-const WelcomePage = ({ history }) => {
+/* Selectors */
+import { getIsLoggedIn } from '../../data/reducers/AuthReducer';
+
+/* Constants */
+import { LANDING_PAGE_ROUTE } from '../../Routes';
+
+const mapStateToProps = state => ({
+  isLoggedIn: getIsLoggedIn(state),
+});
+
+const WelcomePage = ({ history, isLoggedIn }) => {
   const [isUploadingTranscript, setIsUploadingTranscript] = useState(true);
   const [isUploadingSchedule, setIsUploadingSchedule] = useState(false);
+
+  if (!isLoggedIn) {
+    history.push(LANDING_PAGE_ROUTE);
+  }
 
   return (
     <WelcomePageWrapper>
@@ -40,4 +52,4 @@ const WelcomePage = ({ history }) => {
   );
 };
 
-export default withRouter(WelcomePage);
+export default withRouter(connect(mapStateToProps)(WelcomePage));
