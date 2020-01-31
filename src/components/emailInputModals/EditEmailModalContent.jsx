ae4fffcf2201c00073725f065a1137800f4a1f95
Ayush Kapur <31008037+AyushK1@users.noreply.github.com>
2020-01-31 17:36:53 -0500

    messages (#138)

--- src/components/emailInputModals/EditEmailModalContent.jsx
+++ src/components/emailInputModals/EditEmailModalContent.jsx
@@ -5,7 +5,7 @@ import { FormText } from './styles/EmailInputForm';
 const renderText = () => (
   <FormText>
     We will send an alert to the following email when a spot opens up in a
-    section you're subscribed to
+    course section you've subscribed to.
   </FormText>
 );
 
