bd6bd3f6d44a09328d748511f5c443157ed3822c
Ayush Kapur <31008037+AyushK1@users.noreply.github.com>
2020-02-01 12:22:14 -0500

    Update Messages.jsx

--- src/constants/Messages.jsx
+++ src/constants/Messages.jsx
@@ -76,7 +76,7 @@ export const AUTH_SUCCESS = {
 export const SUBSCRIPTION_SUCCESS = {
   unsubscribed: 'Unsubscribed!',
   subscribed:
-    'Subscribed! You’ll receive an email from us when a one spot opens up in this section.',
+    'Subscribed! You’ll receive an email from us when a spot opens up in this section.',
 };
 
 export const REVIEW_SUCCESS = {
