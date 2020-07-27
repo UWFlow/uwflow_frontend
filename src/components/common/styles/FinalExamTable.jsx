249326c983306181e21942568d66a2e97060f5e3
Edwin Zhang <edwinzhang64@gmail.com>
2020-07-26 23:11:18 -0600

    Remove file

--- src/components/common/styles/FinalExamTable.jsx
+++
@@ -1,9 +0,0 @@
-import { Link as RouterLink } from 'react-router-dom';
-import styled from 'styled-components';
-
-import { Link } from 'constants/Mixins';
-
-export const CourseCode = styled(RouterLink)`
-  ${Link}
-  color: ${({ theme }) => theme.courses};
-`;
