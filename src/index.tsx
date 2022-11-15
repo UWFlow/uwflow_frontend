be43e357b9c843c56887938e5eb249cc1a23fb75
Edwin Zhang <edwinzhang64@gmail.com>
2022-11-15 11:54:12 -0700

    S

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import App from 'App';
 import { createBrowserHistory } from 'history';
 import { configureStore } from 'Store';
@@ -14,6 +16,17 @@ import ModalProvider from 'components/modal/ModalProvider';
 import Theme from 'constants/GlobalTheme';
 import client from 'graphql/apollo.js';
 import SearchProvider from 'search/SearchProvider';
+
+Sentry.init({
+  dsn:
+    'https://b7c690df83634243877b9e322c371f08@o4504154328465408.ingest.sentry.io/4504154360643584',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const StartApp = () => {
   const store = configureStore();
