04b99d8daf802248f289ed176a42cca1e1f20fc0
jerry <jerry.zhou@uwaterloo.ca>
2025-03-03 10:18:08 -0800

    Flow 19 - Implemented Sentry Monitoring (#190)

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
+# syntax=docker/dockerfile:1.4
 FROM node:16-alpine AS builder
-
 WORKDIR /work
-
 COPY . .
 RUN yarn install
 RUN yarn lint-nofix
-RUN yarn build
+
+# Use secrets during build time only
+RUN --mount=type=secret,id=sentry_auth_token \
+    export SENTRY_AUTH_TOKEN="$(cat /run/secrets/sentry_auth_token)" && \
+    yarn build
 
 FROM nginx:alpine
 
---
+++ src/sentry.ts
@@ -0,0 +1,24 @@
+import { Route } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+
+import { history } from './browserhistory';
+
+Sentry.init({
+  dsn:
+    'https://4fc1290ffbf3e98712374e16e8cea4ac@o4504154328465408.ingest.us.sentry.io/4508889530236928',
+  integrations: [
+    Sentry.reactRouterV5BrowserTracingIntegration({ history }),
+    Sentry.replayIntegration(),
+  ],
+  tracePropagationTargets: ['localhost'],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+// Optionally export anything from here if needed
+export const SentryRoute = Sentry.withSentryRouting(Route);
