4f960f3801e3c47c969099d45add2e5c1ecbec87
jerry <jerry.zhou@uwaterloo.ca>
2025-10-05 21:12:55 -0400

    FLOW 41 - add banner (#197)
    Added a banner. Probably woefully overengineered. 
    
    
    ![image](https://github.com/user-attachments/assets/b50f3aef-68d4-436a-9b6c-a324f6b22602)
    
    ---------
    
    Co-authored-by: addison-ch <addisonchen.py@gmail.com>

---
+++ src/components/banner/AnnouncementBanner.tsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState } from 'react';
+import FadeIn from 'react-fade-in';
+import { DefaultTheme, useTheme } from 'styled-components';
+
+const AnnouncementBanner = () => {
+  const theme: DefaultTheme = useTheme();
+
+  const bannerRef = useRef<HTMLDivElement>(null);
+
+  // Create a localStorage key based on the banner ID
+  const localStorageKey = `banner-dismissed`;
+
+  const [dismissed, setDismissed] = useState<boolean>(
+    localStorage.getItem(localStorageKey) != null,
+  );
+  const handleDismiss = () => {
+    setDismissed(true);
+    localStorage.setItem(localStorageKey, 'true');
+  };
+
+  // If the banner is dismissed, we return null
+  if (dismissed) {
+    return null;
+  }
+
+  return (
+    <FadeIn>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          padding: '15px',
+          backgroundColor: theme.accent,
+        }}
+      >
+        <div style={{ textAlign: 'center', width: '90%' }}>
+          <strong> UWFlow is open source! </strong> Check out the{' '}
+          <a href="https://github.com/UWFlow/uwflow/releases/tag/v1.0.0">
+            announcement here!
+          </a>
+        </div>
+        <button
+          style={{
+            background: 'none',
+            fontSize: '20px',
+            opacity: '50%',
+            border: 'none',
+            cursor: 'pointer',
+          }}
+          onClick={handleDismiss}
+        >
+          x
+        </button>
+      </div>
+    </FadeIn>
+  );
+};
+
+export default AnnouncementBanner;
